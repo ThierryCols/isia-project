e06b37c0e5697811ce9249dff4e4ef24f59765f6
ThierryCols <thierrycolsenet@gmail.com>
2018-03-27 16:39:40 +0200

    :dollar: add eth support

--- main.py
+++ main.py
@@ -12,15 +12,29 @@ def fetch_data():
         'rates': response.json()['ticker']['markets']
     }
 
-    saved_data = get_data_from_file()
+    saved_data = get_data_from_file('btc')
     saved_data.append(to_save)
 
-    save_data_to_file(saved_data)
+    save_data_to_file(saved_data, 'btc')
+
+    response = requests.get('https://api.cryptonator.com/api/full/eth-usd')
+    to_save = {
+        'rates_date': time(),
+        'rates': response.json()['ticker']['markets']
+    }
+
+    saved_data = get_data_from_file('eth')
+    saved_data.append(to_save)
+
+    save_data_to_file(saved_data, 'eth')
 
 
-def get_data_from_file():
+def get_data_from_file(crypto_name):
     """ load data from file and serve it """
-    data_file = open(r'./data', 'rb')
+    if (crypto_name == 'btc'):
+        data_file = open(r'data', 'rb')
+    else:
+        data_file = open(r'data_eth', 'rb')
     try:
         saved_data = pickle.load(data_file)
     except EOFError:
@@ -29,17 +43,26 @@ def get_data_from_file():
     return saved_data
 
 
-def save_data_to_file(data):
+def save_data_to_file(data, crypto_name):
     """ save data in target file """
-    file_to_save = open(r'./data', 'wb')
+    if (crypto_name == 'btc'):
+        file_to_save = open(r'data', 'wb')
+    else:
+        file_to_save = open(r'data_eth', 'wb')
     file_to_save.truncate()
     pickle.dump(data, file_to_save)
     file_to_save.close()
 
 
-@hug.get('/crypto-lovers')
-def serve_data_over_internet():
-    data = get_data_from_file()
+@hug.get('/btc-lovers')
+def serve_btc_over_internet():
+    data = get_data_from_file('btc')
+    return data
+
+
+@hug.get('/eth-lovers')
+def serve_eth_over_internet():
+    data = get_data_from_file('eth')
     return data
 
 
